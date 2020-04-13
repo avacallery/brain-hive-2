6033f0bc2cacec652e26040685626caa6f0fc197
avacallery <avacallery@gmail.com>
2020-04-13 11:47:16 -0400

    added renderPosts method and mock file with post data

--- brain-hive-2/src/App.js
+++ brain-hive-2/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Post from './components/Post'
 import './App.css';
+import posts from './mock/posts';
 
 //turn functional component into class-based component 
 
@@ -8,33 +9,35 @@ import './App.css';
 //that's how we're going to get props/state/mounting etc 
 
 class App extends Component {
-  //now this is an attribute of the App class
-  //so no const in front of post
-    post = {
-    posterName: 'Ava Callery',
-    resourceAuthor: 'Paul McCartney',
-    jobSkillLevel: 'Junior Developer',
-    cohort: '4',
-    title: 'How To Write A Song About React: Lesson 1',
-    categories: ['Apple', 'react'],
-    summary: 'McCartney sure knows how to write a song but I do not think he knows much about react.',
-    link: 'www.google.com/search?q=PaulMcCartney',
-    resourceType: 'Article',
-    datePublihed: '1965-11-20:00:00:00z',
-    videolength: null,
-    timeToComplete: 10,
-    rating: 3,
-    comments: [{
-        commenter: 'Ava', text: 'Love the Beatles.'
-    },
-    { commenter: 'Ringo', text: 'This is not the real Paul.'}
-    ]
-}
+  //separation of concern and readability
+  //each function or method should do one task
+
+  renderPosts = () => {
+    //theres a lot of tools we can use to loop through our array of posts (.map, .filter, .forEach, or For Loop)
+    //.map iterates through an existing array and creates an entirely new array 
+    //.map does not have to replicate the original length
+    //.map whatever you return in that array is what fills the new array
+    //.map you're reMAPPING the array
+
+    //this function on line 23 is the same as the nameless function below on line 28)
+    // const mapFunction = (post) => {
+    //   return <Post post={post} />;
+    // }
+    // const display = posts.map(mapFunction); 
+
+    //nameless function because we only use it once
+    const display = posts.map(post => {
+      //importing prop from Post component and reassigning
+      return <Post post={post} />
+    })
+    return display;
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to BrainHive-2!</h1>
-        <Post post={this.post}/>
+        {this.renderPosts()}
       </div>
     );
   }
---
+++ brain-hive-2/src/mock/posts.js
@@ -0,0 +1,41 @@
+const posts = [
+    {
+    posterName: 'Ava Callery',
+    resourceAuthor: 'Paul McCartney',
+    jobSkillLevel: 'Junior Developer',
+    cohort: '4',
+    title: 'How To Write A Song About React: Lesson 1',
+    categories: ['Apple', 'react'],
+    summary: 'McCartney sure knows how to write a song but I do not think he knows much about react.',
+    link: 'www.google.com/search?q=PaulMcCartney',
+    resourceType: 'Article',
+    datePublihed: '1965-11-20:00:00:00z',
+    videolength: null,
+    timeToComplete: 10,
+    rating: 3,
+    comments: [
+        {
+      commenter: 'Ava', text: 'Love the Beatles.'
+        },
+        { commenter: 'Ringo', text: 'This is not the real Paul.' }
+    ],
+  }, 
+    {
+    posterName: 'Ava Callery',
+    resourceAuthor: 'Shaun Pelling (The Net Ninja)',
+    jobSkillLevel: 'Advanced',
+    cohort: '8',
+    title: 'Complete React Tutorial (& Redux) #12 - Nesting Components',
+    categories: ['react', 'redux'],
+    summary: "This was a quick video that efficiently shows how to nest components in a React app.",
+    link: 'https://www.youtube.com/watch?v=dQublHlGgBw',
+    resourceType: 'Video',
+    datePublished: '2018-08-10.00:00:00z',
+    videoLength: 5,
+    timeToComplete: 8,
+    rating: 5,
+    comments: [{ commenter: "Shaun Pelling", text: 'Remember to subscribe, mate.' }]
+  }
+];
+
+export default posts;
