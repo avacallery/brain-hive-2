23904e0ad776adc190bd9b05214b387afb4be779
avacallery <avacallery@gmail.com>
2020-04-23 10:39:54 -0400

    set state in PostForm

---
+++ brain-hive-2/src/components/PostForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"; 
+
+class PostForm extends Component {
+    state = {
+        posterName: "",
+        resourceAuthor: "",
+        jobSkillLevel: "",
+        cohort: "",
+        title: "",
+        categories: "",
+        summary: "",
+        link: "",
+        resourceType: "",
+        datePublished: null,
+        videoLength: "",
+        timeToComplete: "",
+        raiting: "",
+        comments: []
+    };
+};
