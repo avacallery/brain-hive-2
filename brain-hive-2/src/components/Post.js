5a96083bbcc4144636376ce657a7d1bdb949b72b
avacallery <avacallery@gmail.com>
2020-04-26 15:46:04 -0400

    change Post.js into stateful component

--- brain-hive-2/src/components/Post.js
+++ brain-hive-2/src/components/Post.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'; //if the item is clicked, user will be redirected
 
-//no render method in a functional component
+//the post needs to know it is clicked by keeping track of the state(clicked or unclicked)
+//in order to access the state for Redirect, we make Post.js into a stateful component
 
-const Post = ({post, handleSelect}) => {
+class Post extends Component {
+    render() {
+        const { post, handleSelect } = this.props;
 
-    const handleClick = () => {
-        //we want to send back the id of the post we are on
-        handleSelect(post.id);
-    }
+        const handleClick = () => {
+            //we want to send back the id of the post we are on
+            handleSelect(post.id);
+        }
 
-    return (
-        <div className={'box'} onClick={() => handleClick()}>
-            <div className={'postTitle'}>
-            <h2>{post.title}</h2>
+        return (
+            <div
+                className={'box'}
+                onClick={() => handleClick()}>
+                <div className={'postTitle'}>
+                    <h2>{post.title}</h2>
+                </div>
+                <b>{post.resourceAuthor}</b>
+                <p>Poster: {post.posterName}</p>
+                {post.videoLength ? (<p>Length: {post.videolength}</p>)
+                    : null
+                }
+                <i>"{post.summary}"</i>
+                <p>Comments: {post.comments.length}</p>
             </div>
-            <b>{post.resourceAuthor}</b>
-            <p>Poster: {post.posterName}</p>
-            {   post.videoLength ? (<p>Length: {post.videolength}</p>) 
-                : null
-            }
-            <i>"{post.summary}"</i>
-            <p>Comments: {post.comments.length}</p>
-        </div>
-    )
+        )
+    }
 }
 
 export default Post;
