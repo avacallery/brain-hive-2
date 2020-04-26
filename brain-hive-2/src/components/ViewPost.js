2dd329f96ea2f6dcf99d78f1916ce36646d96c4a
avacallery <avacallery@gmail.com>
2020-04-26 16:27:26 -0400

    Tried to make ViewPost into stateful component and failed

--- brain-hive-2/src/components/ViewPost.js
+++ brain-hive-2/src/components/ViewPost.js
@@ -1,12 +1,29 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
 import { useParams } from 'react-router-dom';
 
-const ViewPost = (props) => {
-    const { postId } = useParams(); 
-    const { post } = props; 
+class ViewPost extends Component { 
+    state = {
+    }; 
+    //how do we bring in props? 
+    render() {
+    // useParams();
+    const { post } = this.props;
+
     return(
-    <h3>{post.title}</h3>
+    <div className="post" style={myPostStyle.post}>
+    <h3>{this.props.title}</h3>
+    <h2>Author: {this.props.resourceAuthor}</h2>
+    </div>
     );
+}
+}
+
+const myPostStyle = {
+    post: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    }
 }
 
 export default ViewPost; 
