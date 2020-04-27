babebe7de94075c682d5e846aa95af8f19c74c7e
avacallery <avacallery@gmail.com>
2020-04-27 12:23:33 -0400

    add ViewPost elements to display

--- brain-hive-2/src/components/ViewPost.js
+++ brain-hive-2/src/components/ViewPost.js
@@ -1,12 +1,23 @@
-import React from 'react'; 
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const ViewPost = (props) => {
+    //extract postid from parameter
     const { postId } = useParams(); 
     const { post } = props; 
+
     return(
-    <h3>{post.title}</h3>
-    );
-}
+        <div className="selectedPost">
+        <h1>{post.title}</h1>
+        <h2>{post.resourceAuthor}</h2>
+        <h4>Submitted by: {post.posterName}, Cohort: {post.cohort}</h4>
+        <h4>Rating: {post.rating}</h4>
+        <i>"{post.summary}"</i>
+        <p>Time to complete: {post.timeToComplete} minutes</p>
+        <a href={post.link}>Click to view article.</a>
+        </div>
+    )
+    //.map you will use to display comments
+};
 
 export default ViewPost; 
