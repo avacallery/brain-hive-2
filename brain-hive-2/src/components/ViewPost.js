e8ed731201e268f55fd54b675bdeb30aad52ce19
avacallery <avacallery@gmail.com>
2020-05-04 13:29:26 -0400

    view article link

--- brain-hive-2/src/components/ViewPost.js
+++ brain-hive-2/src/components/ViewPost.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-const ViewPost = (props) => {
-    //extract postid from parameter
-    const { postId } = useParams(); 
-    const { post } = props; 
+class ViewPost extends Component {
+    state = {
+        showComments: false
+    }
 
-    return(
-        <div className="selectedPost">
-        <h1>{post.title}</h1>
-        <h2>{post.resourceAuthor}</h2>
-        <h4>Submitted by: {post.posterName}, Cohort: {post.cohort}</h4>
-        <h4>Rating: {post.rating}</h4>
-        <i>"{post.summary}"</i>
-        <p>Time to complete: {post.timeToComplete} minutes</p>
-        <a href={post.link}>Click to view article.</a>
-        </div>
-    )
+    clickHandler = () => {
+        this.setState({
+            showComments: !this.state.showComments
+        });
+    }
+
+    renderComments = (post) => {
+        return post.comments.map((comment) => {
+            return (
+                <div key={comment.commenter} className="box">
+                    <p>{comment.commenter}</p>
+                    <p>{comment.text}</p>
+                </div>
+            );
+        });
+    };
+
+    render() {
+        return (
+            <div className="selectedPost">
+                <h1>{post.title}</h1>
+                <h2>{post.resourceAuthor}</h2>
+                <h4>Submitted by: {post.posterName}, Cohort: {post.cohort}</h4>
+                <h4>Rating: {post.rating}</h4>
+                <i>"{post.summary}"</i>
+                <p>Time to complete: {post.timeToComplete} minutes</p>
+                <a href={`http://${post.link}`}>Click to view article.</a>
+            </div>
+        )
+    };
     //.map you will use to display comments
 };
 
