398dbdc9a9e06c9411ac40d1ccbbdd01f9a21324
avacallery <avacallery@gmail.com>
2020-05-07 10:42:32 -0400

    turn PostList into class based component

--- brain-hive-2/src/components/PostList.js
+++ brain-hive-2/src/components/PostList.js
@@ -1,18 +1,50 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import Post from './Post'; 
 
-const PostList = (props) => {
-    const renderPosts = () => {
-        const display = props.posts.map((post) => {
-            return <Post post={post} 
-            key={post.id} 
-            handleSelect = {props.handleSelect}/>
-        })
-        return display; 
-    };
-    return (
-        <div className="postList">{renderPosts()}</div>
-    );
+class PostList extends Component {
+//where are these props coming from and what are they?
+    state = {
+        query: "", 
+        filteredPosts: [...this.props.posts],
+    }
+
+   handleChange = (e) => {
+       //query the updated text from input
+       const query = e.target.value;
+
+        const newPosts = this.props.posts.filter((post) => {
+            //why are we using >= 0 what does 0 represent? 
+            if (post.title.toLowerCase().indexOf(query.toLowercase()) >= 0) {
+                return true; 
+            }
+            if (post.summary.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
+                return true; 
+            }
+                return false; 
+        });
+
+        this.setState({
+            query: query, 
+            filteredPosts: newPosts; 
+        });
+   }
+
+   renderPosts = () => {
+       const display = this.state.filteredPosts.map((post) => {
+           return <Post 
+           post={post} 
+           key={post.id} 
+           handleSelect={this.props.handleSelect} />
+       });
+       return display; 
+   };
+
+   render() {
+       return (
+           
+       )
+   }
+
 };
 
 export default PostList; 
