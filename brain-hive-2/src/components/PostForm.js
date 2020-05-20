a791ff41d11427bb8ca3cbc0ecaea790ac5b3986
avacallery <avacallery@gmail.com>
2020-05-20 19:24:39 -0400

    PostForm redux issue

--- brain-hive-2/src/components/PostForm.js
+++ brain-hive-2/src/components/PostForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { changeForm, submitForm } from '../actions';
-import Button from './common/Button';
+import Button from './Button';
 
 const PostForm = (props) => {
   const { form } = props.newPost;
