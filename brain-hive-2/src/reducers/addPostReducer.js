b14b37b51fbba8c185c5951f121d726b97b0026c
avacallery <avacallery@gmail.com>
2020-05-20 19:16:54 -0400

    add post reducer with cases CHANGE_FORM and SUBMIT_FORM

---
+++ brain-hive-2/src/reducers/addPostReducer.js
@@ -0,0 +1,43 @@
+import { CHANGE_FORM, SUBMIT_FORM } from '../actions/types';
+
+const INITIAL_STORE = {
+  form: {
+    id: 0,
+    posterName: '',
+    resourceAuthor: '',
+    jobSkillLevel: '',
+    cohort: '',
+    title: '',
+    categories: '',
+    summary: '',
+    link: '',
+    resourceType: '',
+    datePublished: null,
+    videoLength: '',
+    timeToComplete: '',
+    raiting: '',
+    comments: [],
+  },
+  loading: false,
+  errors: null,
+};
+
+export default (store = INITIAL_STORE, action) => {
+  switch (action.type) {
+    case CHANGE_FORM:
+      return {
+        ...store,
+        form: {
+          ...store.form,
+          [action.payload.field]: action.payload.value,
+        },
+      };
+
+    case SUBMIT_FORM:
+      return {
+        ...INITIAL_STORE,
+      };
+    default:
+      return store;
+  }
+};
